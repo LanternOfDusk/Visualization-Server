41ee72153b7d95cf5ec7447327d29b368d5c33a9
WooJinLim <95209789+woojin2296@users.noreply.github.com>
2024-05-11 19:05:17 +0900

    FEAT : 키보드 컨트롤 수정 및 마우스 컨트롤 기능 추가

--- lanternofdusk/src/views/ControlView.vue
+++ lanternofdusk/src/views/ControlView.vue
@@ -10,51 +10,38 @@ export default {
   setup() {
     const threeContainer = ref(null);
 
-    let scene, camera, renderer, animationFrameId;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
     const keyStates = {};
 
+    let frameId;
+    
     const initThree = () => {
-      // 장면 초기화
-      scene = new THREE.Scene();
-
-      // 카메라 설정
-      camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.set(0, 1.6, 5); // 카메라 위치 설정
+      // scene 설정
+      scene.background = new THREE.Color(0xffffff);
+      
+      // camera 설정
+      camera.position.set(0, 20, 80);
       camera.rotation.order = 'YXZ';
 
-      // 조명 설정
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // 주변광
-      scene.add(ambientLight);
-
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // 방향광
-      directionalLight.position.set(5, 10, 7.5);
-      scene.add(directionalLight);
-
-      // 바닥 설정
-      const planeGeometry = new THREE.PlaneGeometry(20, 20);
-      const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa, side: THREE.DoubleSide });
-      const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-      plane.rotation.x = -Math.PI / 2; // 바닥을 수평으로 설정
-      scene.add(plane);
-
       // 렌더러 설정
-      renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
       renderer.setPixelRatio(window.devicePixelRatio);
-
       if (threeContainer.value) {
         threeContainer.value.appendChild(renderer.domElement);
       }
+
+      // 바닥 설정
+      const grid = new THREE.GridHelper(100, 20);
+      scene.add( grid );
 
       // 이벤트 핸들러 설정
       window.addEventListener('resize', onWindowResize);
       document.addEventListener('keydown', onKeyDown);
       document.addEventListener('keyup', onKeyUp);
-
-      // 카메라 제어용
-      document.body.addEventListener('mousemove', onMouseMove);
-
-      render();
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('click', onClick);
     };
 
     const onWindowResize = () => {
@@ -67,7 +54,6 @@ export default {
 
     const onKeyDown = (event) => {
       keyStates[event.code] = true;
-      console.log(event.code);
     };
 
     const onKeyUp = (event) => {
@@ -81,44 +67,77 @@ export default {
       }
     };
 
-    const render = () => {
-      animationFrameId = requestAnimationFrame(render);
-      renderer.render(scene, camera);
-
-      // 카메라 제어
-      if (keyStates['KeyW']) {
-        camera.position.z -= 0.05; // 앞쪽으로 이동
+    const onClick = () => {
+      if (document.pointerLockElement === null) {
+        document.body.requestPointerLock();
       }
-      if (keyStates['KeyS']) {
-        camera.position.z += 0.05; // 뒤로 이동
-      }
-      if (keyStates['KeyA']) {
-        camera.position.x -= 0.05; // 좌로 이동
-      }
-      if (keyStates['KeyD']) {
-        camera.position.x += 0.05; // 우로 이동
-      }
-      if (keyStates['Space']) {
-        camera.position.y += 0.05; // 위로 이동
-      }
-      if (keyStates['ShiftLeft']) {
-        camera.position.y -= 0.05; // 위로 이동
+      else {
+        document.exitPointerLock();
       }
     };
 
+    const render = () => {
+      frameId = requestAnimationFrame(render);
+      renderer.render(scene, camera);
+
+      updateCamera();
+    };
+
+    const updateCamera = () => {
+      let speed = keyStates['ControlLeft'] ? 1 : 0.5;
+
+      if (keyStates['KeyW']) {
+        camera.position.add(getForwardVector().multiplyScalar(speed));
+      }
+      if (keyStates['KeyS']) {
+        camera.position.add(getForwardVector().multiplyScalar(-speed));
+      }
+      if (keyStates['KeyA']) {
+        camera.position.add(getSideVector().multiplyScalar(-speed));
+      }
+      if (keyStates['KeyD']) {
+        camera.position.add(getSideVector().multiplyScalar(speed));
+      }
+      if (keyStates['Space']) {
+        camera.position.y += 0.5;
+      }
+      if (keyStates['ShiftLeft']) {
+        camera.position.y -= 0.5;
+      }
+    }
+
+    function getForwardVector() {
+      let cameraDirection = new THREE.Vector3();
+      camera.getWorldDirection(cameraDirection);
+      cameraDirection.y = 0;
+      cameraDirection.normalize();
+      return cameraDirection;
+    }
+
+    function getSideVector() {
+      let cameraDirection = new THREE.Vector3();
+      camera.getWorldDirection(cameraDirection);
+      cameraDirection.y = 0;
+      cameraDirection.normalize();
+      cameraDirection.cross(camera.up);
+      return cameraDirection;
+    }
+
     onMounted(() => {
       initThree(); // 컴포넌트가 마운트되면 초기화
+      render(); // 랜더링 시작
     });
 
     onUnmounted(() => {
       // 컴포넌트가 언마운트되면 정리 작업
-      if (animationFrameId !== null) {
-        cancelAnimationFrame(animationFrameId);
+      if (frameId !== null) {
+        cancelAnimationFrame(frameId);
       }
       window.removeEventListener('resize', onWindowResize);
       document.removeEventListener('keydown', onKeyDown);
       document.removeEventListener('keyup', onKeyUp);
-      document.body.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('click', onClick);
     });
 
     return {
